4462b1a3697fae4d1ff382900b5cc13c25e4097f
Nishka Ranadive <nishka.amol.aug2006@gmail.com>
2025-09-12 23:24:57 +0530

    Add files via upload

---
+++ exp-13_1.cpp
@@ -0,0 +1,33 @@
+//Name: Nishka Ranadive
+//PRN: 24070123082
+//Exp-13
+
+//Constructor Overloading
+//Program-1
+#include<iostream>
+using namespace std;
+
+class Add{
+    public:
+
+    Add(int a, int b){
+        cout<<"Sum is(int): "<<a+b<<endl;
+    }
+
+    Add(float a, float b){
+        cout<<"Sum is(double): "<<a+b<<endl;
+    }
+
+    Add(int a, int b, int c){
+        cout<<"Sum is(3 digits): "<<a+b+c<<endl;
+    }
+};
+
+int main(){
+
+    Add a1(2, 3);
+    Add a2(1.2f, 2.1f);
+    Add a3(1, 2, 3);
+
+    return 0;
+}
